d2a5f2d9ee49d92e811ecc8d9e74f770a5c47a68
Maria Kazakova <mariakazakova92@gmail.com>
2025-02-05 17:17:16 +0300

    added label under the input

--- src/shared/ui/SearchInput/SearchInput/SearchInput.tsx
+++ src/shared/ui/SearchInput/SearchInput/SearchInput.tsx
@@ -2,6 +2,7 @@ import s from './searchInput.module.css'
 import {ChangeEvent, useState} from "react";
 import {debounce} from "../../../utils/debounce.ts";
 import {getCharacters} from "../../../api/sendRequests.ts";
+import {CharacterResponse} from "../../../types/types.ts";
 
 const LIMIT_NUMBER = 3;
 
@@ -17,25 +18,22 @@ export const SearchInput = () => {
     const debounceSendRequest = debounce((query: string) => {
         if (query.length > LIMIT_NUMBER) {
             console.log("count symbol", symbol)
-            getCharacters(query).then((data) => {
+            getCharacters(query).then((data: CharacterResponse) => {
                 console.log("RequestData", data)
-                setCountCharacters()
+                setCountCharacters(data.info.count)
             })
         }
     }, 500)
 
 
     return (
-        <>
-            <div className={s.inputWrapper}>
-                <input type="text" placeholder="Search characters..." className={s.input} autoFocus={true}
-                       value={symbol} onChange={(e) => handleInputChange(e)}/>
-            </div>
+        <div className={s.inputWrapper}>
+            <input type="text" placeholder="Search characters..." className={s.input} autoFocus={true}
+                   value={symbol} onChange={(e) => handleInputChange(e)}/>
             {
-                countCharacters !== null && <p>Found characters: {countCharacters}</p>
+                countCharacters !== null && <p className={s.label}>Found characters: {countCharacters}</p>
             }
-
-        </>
+        </div>
 
     )
 }
